cf96bffa853f7eff672141c099cba9211c1a57be
sleroy <sylvain.leroy@tocea.com>
2017-05-03 23:02:04 +0200

    Fixed a compilation error

--- src/test/java/com/github/sleroy/junit/mail/server/MailSaverTest.java
+++ src/test/java/com/github/sleroy/junit/mail/server/MailSaverTest.java
@@ -6,9 +6,9 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -124,7 +124,7 @@ public class MailSaverTest {
 		MailServerModel mailServerModel = new MailServerModel();
 		ServerConfiguration serverConfiguration = new ServerConfiguration();
 
-		serverConfiguration.relayDomains((List) null);
+		serverConfiguration.relayDomainsList((List) null);
 		MailSaver mailSaver = new MailSaver(serverConfiguration);
 
 		// AND I don't forget to register the mailServerModel
